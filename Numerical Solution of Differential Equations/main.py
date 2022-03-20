5fdf4af8b7104feb14ed231d65e2bcf9d9e58693
Sergey Dolzhenkov <sergeypost1946@gmail.com>
2022-03-20 21:03:10 +0300

    update RODAS coefficients

--- Numerical Solution of Differential Equations/main.py
+++ Numerical Solution of Differential Equations/main.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     # solve_SDE_1(start=0., end=0.3, step=0.01, method="BEM")
     # solve_SDE_1(start=0., end=0.3, step=0.001, method="ITRM")
     # solve_SDE_1(start=0., end=0.3, step=0.001, method="SDIRKM")
-    # solve_SDE_1(start=0., end=0.3, step=0.001, method="ROSM")
+    # solve_SDE_1(start=0., end=0.3, step=0.01, method="ROSM")
 
     # solve_SDE_2(start=0., end=0.001, step=0.00001, method="EM")
     # solve_SDE_2(start=0., end=0.001, step=0.000003, method="RKM")
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     # solve_DAE(start=0., end=0.2, step=0.0001, method="BEM")
     # solve_DAE(start=0., end=0.2, step=0.0001, method="ITRM")
     # solve_DAE(start=0., end=0.2, step=0.0001, method="SDIRKM")
-    # solve_DAE(start=0., end=0.2, step=0.0001, method="ROSM")
+    # solve_DAE(start=0., end=0.2, step=0.001, method="ROSM")
 
     calculate_RODAS_coefficients()
 
